6e36f2b271a59c6b6ad690c4228f41282bbc659c
zjsan <zjsantos25@gmail.com>
2024-11-21 11:20:03 +0800

    testing login in the THE Website

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import Select
+import logging
 import time 
 import pandas as pd
 import numpy as np
@@ -13,21 +14,32 @@ import numpy as np
 service = Service(executable_path = "chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://google.com")
+# Login credentials
+username = "jpacang@mmsu.edu.ph"
+password = "awanpasswordna"
 
-WebDriverWait(driver,5).until(
-    EC.presence_of_element_located((By.XPATH, "//div[@id='SIvCob']"))
-)
+wait = WebDriverWait(driver, 10)
+
 try:
-    element = driver.find_element(By.XPATH, "//div[@id='SIvCob']")
-    print(element.text)
-    print("Scrapping success")
-    input_element = driver.find_element(By.XPATH, "//textarea[@class='gLFyf']")
-    input_element.clear()
-    input_element.send_keys("tech with tim" + Keys.ENTER)
+    driver.get("https://auth.timeshighereducation.com/auth/realms/THE/protocol/openid-connect/auth?client_id=DataPoints&redirect_uri=https%3A%2F%2Fwww.timeshighereducation.com%2Fdatapoints%2F&state=b7eafe6e-0f6f-4b21-8950-dcda25d30a03&response_mode=fragment&response_type=code&scope=openid&nonce=79c86488-b331-4a3a-848b-bceee80cecab")
+
+    el = wait.until(EC.element_to_be_clickable((By.ID, "username")))
+    el.send_keys(username)  # Enter username
+
+    el = wait.until(EC.element_to_be_clickable((By.ID, "password")))
+    el.send_keys(password)  # Enter password
+
+    el = wait.until(EC.element_to_be_clickable((By.ID, "kc-login")))
+    el.click()
+
+    print("Waiting for the profile icon or similar element to confirm login...")
+    driver.get("https://www.timeshighereducation.com/datapoints/")
+    print(driver.title)#check for successful login
+    print("Login successful!")
+
 except Exception as e:
-    print(f"Error: {e}")
-    print("error")
+    logging.error(f"Error in login found: {e}")
+    print(driver.page_source)
 finally:
-    #time.sleep(10)
-    driver.quit
+    driver.quit()
+    
