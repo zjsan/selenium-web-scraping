ba00f4672574a6db0decead2bbb017945822cc1c
zjsan <zjsantos25@gmail.com>
2024-11-21 11:17:06 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import Select
+import time 
+import pandas as pd
+import numpy as np
+
+service = Service(executable_path = "chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://google.com")
+
+WebDriverWait(driver,5).until(
+    EC.presence_of_element_located((By.XPATH, "//div[@id='SIvCob']"))
+)
+try:
+    element = driver.find_element(By.XPATH, "//div[@id='SIvCob']")
+    print(element.text)
+    print("Scrapping success")
+    input_element = driver.find_element(By.XPATH, "//textarea[@class='gLFyf']")
+    input_element.clear()
+    input_element.send_keys("tech with tim" + Keys.ENTER)
+except Exception as e:
+    print(f"Error: {e}")
+    print("error")
+finally:
+    #time.sleep(10)
+    driver.quit
