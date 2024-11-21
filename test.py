66cd2deea1429ac26d5c8dce62ccf24a6377f7c6
zjsan <zjsantos25@gmail.com>
2024-11-21 11:15:23 +0800

    renamed main.py

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time 
 
 service = Service(executable_path = "chromedriver.exe")
@@ -9,13 +11,19 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
 
+WebDriverWait(driver,5).until(
+    EC.presence_of_element_located((By.XPATH, "//div[@id='SIvCob']"))
+)
 try:
     element = driver.find_element(By.XPATH, "//div[@id='SIvCob']")
     print(element.text)
     print("Scrapping success")
+    input_element = driver.find_element(By.XPATH, "//textarea[@class='gLFyf']")
+    input_element.clear()
+    input_element.send_keys("tech with tim" + Keys.ENTER)
 except Exception as e:
     print(f"Error: {e}")
     print("error")
 finally:
-    time.sleep(10)
+    #time.sleep(10)
     driver.quit
