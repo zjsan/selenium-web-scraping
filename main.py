13267602c963710735da0dc269d74750a4fb557c
aroldwin13 <aroldwin13@gmail.com>
2024-11-21 09:10:35 +0800

    installed selenium and chrome driver and set-up virtual environment

---
+++ main.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service 
+
+service = Service(executable_path = "chromedriver.exe")
+driver = webdriver.Chrome(service=service)
