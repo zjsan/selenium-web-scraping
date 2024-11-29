afbd4414ebc794a5e97e92f2915413c3c0d66ba7
zjsan <zjsantos25@gmail.com>
2024-11-29 10:32:19 +0800

    successfully consolidated the sheets

--- merged.py
+++ merged.py
@@ -16,21 +16,26 @@ try:
     merged_data = pd.DataFrame()
     print("---Available excel files: ---",excel_files)
     print("\n")
+    
     # Loop through each file and append its content
     for file in excel_files:
         #file_path = os.path.join(directory, file)#get excel file in excel_files
         worksheets = pd.ExcelFile(str(directory) + "/" + file)
         sheets = worksheets.sheet_names
         print(sheets)
-        # if file.startswith("SDG Overall")
-        # data = pd.read_excel(file_path)#read excel file
-        # merged_data = pd.concat([merged_data, data], ignore_index=True)#store excel in the merged_data 
+        
+        for sheet in sheets:
+            if sheet.startswith("SDG Overall"):   
+                data = pd.read_excel(str(directory) + "/" + file,sheet_name = sheet)#read sheet
+                merged_data = pd.concat([merged_data, data], ignore_index=True)
+                print("\n",sheet)
+                
     print("\nsuccess displaying sheets")
-    # # Save the merged file
-    # merged_file_path = os.path.join(directory, 'Merged_Universities_Data.xlsx')
-    # merged_data.to_excel(merged_file_path, index=False)
+    # Save the merged file
+    merged_file_path = os.path.join(directory, 'Merged_Universities_Data2.xlsx')
+    merged_data.to_excel(merged_file_path, index=False)
 
-    # print(f"Merged file saved at: {merged_file_path}")
+    print(f"Merged file saved at: {merged_file_path}")
 except Exception as e:
     print(f"Error: {e}\n")
     print("----Excel files can't be merged----")
